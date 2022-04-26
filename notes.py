e5b99d4a205241cc3e84fa069ce47c8a41689e9f
joh720 <91515821+joh720@users.noreply.github.com>
2022-04-25 23:04:45 -0400

    Add files via upload

---
+++ notes.py
@@ -0,0 +1,48 @@
+"""
+this is my notes for python- beginner python inside sololearn
+
+remember that to add spaces you use the double quotes with the space in between them.
+e.g. " "
+
+MATH- remember integers are whole numbers and floats are decimals, for exponents use the ** e.g. (2**5) using exponents with floats e.g. (9 ** (1/2)). 
+
+Floor division is the same as normal division and we express that with the double slashes //. 
+
+For the remainders you use the "modulo operator" aka " % " this will only give you the remainder, so if you want to get both you quotient and the reaminder you should run both on different lines
+
+STRINGS
+
+texts are called strings
+
+when you need a new line add ot the code the \n so you can add a new line. Use the \t to tab the line
+
+use a backslash when wanting to use single quotes in double quotes e.g. ('Brian/'s mother)
+you can also use the (\""") to have each line be auto created. Remember to add each string to a different line this just makes it so you dont have to use the ("\n")
+
+
+concatenation in python works by adding strings together like you would floats and integers e.g.
+Print ("spam" + 'eggs')
+Print ("2" + "2")
+
+You can multiply strings to integers and it will produce a repeated version of the original string
+
+print("spam" * 3)
+print (4 * '2')
+
+the escape characters are the backslashes "\"
+
+variables- only allow letters, numbers, or underscores. cannot use special characters
+
+e.g. users="Jose"
+e.g. A_NAME_LAST
+e.g. print (users)
+e.g. print (users + 3)
+e.g. users ="Jose" also users="This is a string" so you enter the two different variables for users as "print (users + "!" )
+
+****************************************
+This section will be for udemy learning 
+****************************************
+
+"""
+
+print(len("95637+12"))
